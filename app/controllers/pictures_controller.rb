4be6e51eec85cdff0ce653fc497f3a0a1a5da30d
Jenny Zhang <jzhang23@chicagobooth.edu>
2018-10-27 00:17:51 +0000

    rcav basic
    set up rcavs

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,31 @@
+class PicturesController < ApplicationController
+
+def index
+    render("pic_templates/all_photos.html.erb")
+end
+
+def display_blank_form
+    render("pic_templates/blank_photo_form.html.erb")
+end
+
+def add_photo_to_list
+    render("pic_templates/new_photo_added.html.erb")
+end
+
+def details_one_row
+    render("pic_templates/details_one_row.html.erb")
+end
+
+def edit_photo
+    render("pic_templates/form_edit_photo.html.erb")
+end
+
+def update_photo_info
+    render("pic_templates/updated_photo")
+end
+
+def delete_photo
+    render("pic_templates/photo_deleted.html.erb")
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  
+  get("/photos" ,  {:controller => "pictures" , :action => "index"})
+  
+  get("/photos/new" ,  {:controller => "pictures" , :action => "display_blank_form"})
+  
+  get("/create_photo" , {:controller => "pictures", :action => "add_photo_to_list"})
+  
+  get("/photos/4" , {:controller => "pictures" , :action => "details_one_row"})
+  
+  get("/photos/4/edit" , {:controller => "pictures" , :action => "edit_photo"})
+  
+  get("/update_photo/4" , {:controller => "pictures" , :action => "update_photo_info"})
+  
+  get("/delete_photo/4" , {:controller => "pictures" , :action => "delete_photo"})
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
